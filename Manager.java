dbe416aafdd0c11b453572456c2bf12b1cc38518
KT <nicholas.lobao@ndus.edu>
2025-03-17 16:20:39 -0500

    account for leap year in maxDay method

--- Manager.java
+++ Manager.java
@@ -29,6 +29,8 @@ public class Manager {
     private int getMaxDay() { //helper method that returns the highest day in the month
         if (currentMonth == 2)
             return 28;
+		else if (currentMonth == 2 && ((currentYear % 4 == 0) && (currentYear % 100 != 0 || currentYear % 400 == 0)) //account for leap year
+			return 29;
         else if (currentMonth == 4 || currentMonth == 6 || currentMonth == 9 || currentMonth == 11)
             return 30;
         else
@@ -92,7 +94,7 @@ public class Manager {
 	}
     //returns upcoming shipments within a certain timeframe
     public String getUpcomingShipments(String timeframe) {
-        throw new UnsupportedOperationException("not implemented yet");
+        throw new UnsuppaddortedOperationException("not implemented yet");
     }
     //returns all upcoming shipments
     public String getUpcomingShipments() {
